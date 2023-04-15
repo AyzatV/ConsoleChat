c07e7bd188687fcc8533866b0d5953cde531f8f4
Iliy <dudnikov26@mail.ru>
2023-04-15 23:56:50 +0300

    Add equality and output operators for User

--- Users.cpp
+++ Users.cpp
@@ -13,4 +13,25 @@ namespace aiafpb1
 
 	// "полное" имя пользователя в формате (имя--логин) для возможности идентификации, т.к. имена пользователей могут совпадать
 	std::string User::fullName() const { return (name + "--" + log); }
+
+	User& User::operator=(const User& u)
+	{
+		if (&u != this)
+		{
+			log = u.log;
+			pword = u.pword;
+			name = u.name;
+			last_seen = u.last_seen;
+		}
+		return *this;
+	}
+
+	// вывод в поток данных пользователя, используется для проверки при регистрации пользователя
+	std::ostream& operator<<(std::ostream& out, const User& u)
+	{
+		out << " логин: " << u.log << std::endl;
+		out << "пароль: " << u.pword << std::endl;
+		out << "   имя: " << u.name << std::endl;
+		return out;
+	}
 }
