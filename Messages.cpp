5003bf6d4cd715e541d63532f6849f36b3e6e603
Андрей Курбанов <kurban1111@mail.ru>
2023-04-15 21:20:50 +0300

    Outputting the message parameters to the stream used to display the list of messages

--- Messages.cpp
+++ Messages.cpp
@@ -14,5 +14,22 @@ namespace aiafpb1
 		}
 		return *this;
 	}
+	// вывод параметров сообщения в поток, используется для вывода списка сообщений
+	std::ostream& operator<<(std::ostream& out, const Message& m)
+	{
+		out << std::endl << "отправлено " << m.time << " пользователем [" << m.from_name << "]";
+		if (m.to_name.empty()) { out << " для всех пользователей:"; }
+		else
+		{
+			out << " для пользователя [" << m.to_name << "]:";
+			if (!m.fresh) { out << std::endl << "[сообщение прочитано получателем]"; }
+		}
+		out << std::endl << m.text << std::endl;
+		return out;
+	}
+
+	Messages::Messages() {}
+	Messages::~Messages() {}
+
 
 }
