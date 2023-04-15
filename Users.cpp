76b5e0f817f7f84f1de366826fb67bc53fee9e98
Iliy <dudnikov26@mail.ru>
2023-04-15 23:51:10 +0300

    Add clear, fullName for User

--- Users.cpp
+++ Users.cpp
@@ -2,4 +2,15 @@
 
 namespace aiafpb1
 {
+	// приведение параметров к начальным "нулевым" значениям
+	void User::clear()
+	{
+		this->log.clear();
+		this->pword.clear();
+		this->name.clear();
+		this->last_seen.clear();
+	}
+
+	// "полное" имя пользователя в формате (имя--логин) для возможности идентификации, т.к. имена пользователей могут совпадать
+	std::string User::fullName() const { return (name + "--" + log); }
 }
