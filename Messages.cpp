cfa08a14eed8e7f113f03cedbba5d8c9b5e8317e
Андрей Курбанов <kurban1111@mail.ru>
2023-04-16 21:13:47 +0300

    Added overloaded assignment operator for Message class

---
+++ Messages.cpp
@@ -0,0 +1,19 @@
+﻿#include "Messages.h"
+
+namespace aiafpb1
+{
+	Message& Message::operator=(const Message& m)
+	{
+		if (&m != this)
+		{
+			text = m.text;
+			from_name = m.from_name;
+			to_name = m.to_name;
+			time = m.time;
+			fresh = m.fresh;
+		}
+		return *this;
+	}
+
+	
+}
